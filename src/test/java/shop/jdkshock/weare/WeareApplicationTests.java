6a780eccdadeac57b0f8194296f89c2c8a406cef
ms0130k <minseok0130@gmail.com>
2023-05-22 06:29:36 +0900

    init

---
+++ src/test/java/shop/jdkshock/weare/WeareApplicationTests.java
@@ -0,0 +1,16 @@
+package shop.jdkshock.weare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+class WeareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
