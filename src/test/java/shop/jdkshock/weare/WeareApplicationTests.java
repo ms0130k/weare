cae6b6d894771c664cba43e412f7f0c70d86faec
KimDonghyeon <minseok0130@gmail.com>
2023-05-22 10:32:57 +0900

    default profile 설정

--- src/test/java/shop/jdkshock/weare/WeareApplicationTests.java
+++ src/test/java/shop/jdkshock/weare/WeareApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("dev")
 class WeareApplicationTests {
 
     @Test
