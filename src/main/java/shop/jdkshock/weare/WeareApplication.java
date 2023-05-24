4ca2bddf21fe6dde01a90664d5aed97d1a2bd57c
KimDonghyeon <minseok0130@gmail.com>
2023-05-24 21:07:10 +0900

    프로젝트에서 운영 내용을 변경한 상황

--- src/main/java/shop/jdkshock/weare/WeareApplication.java
+++ src/main/java/shop/jdkshock/weare/WeareApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeareApplication {
 
     public static void main(String[] args) {
-        System.out.println("merge 테스트");
+        System.out.println("merge 테스트 프로젝트에서 운영의 소스 변경한 상황");
         SpringApplication.run(WeareApplication.class, args);
     }
 
